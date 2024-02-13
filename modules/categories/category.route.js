6238e6c2b035cb6ddea7edc638d57bde5e98477c
kubs0135 <kubskathayat01@gmail.com>
2024-02-13 16:38:44 +0545

    did remaining postman work of blogs and also code and postman of categories

--- modules/categories/category.route.js
+++ modules/categories/category.route.js
@@ -1,43 +1,43 @@
 const router=require("express").Router();
-//GET ALL THE USERS
+//GET ALL THE categories
 router.get("/",(req,res)=>{
     const{limit,page,search}=req.query; // used for search, sorting and filter
     //database operation
-    res.json({msg:"Hello from blog route"});
+    res.json({msg:"Hello from category route"});
 });
 
 
-
-//add new user
+//add new category
 router.post("/",(req,res)=>{
     console.log(req.body);
     //database operation
-    res.json({msg:"Hello from blog post route"});
+    res.json({msg:"Hello from category post route"});
 });
 
-//update single user for more than 2 fields
+//update single category for more than 2 fields
 router.put("/:id",(req,res)=>{
     const{id}=req.params;
     const data=req.body;
     console.log({id,data});
     //database operation
-    res.json({msg:`Hello from blog put route ID ${id}`});
+    res.json({msg:`Hello from category put route ID ${id}`});
 });
 
-//update single user for single fields
+
+//update single category for single fields
 router.patch("/:id",(req,res)=>{
     const{id}=req.params;
     const data=req.body;
     console.log({id,data});
     //Database operation
-    res.json({msg:`Hello from blog patch route ID ${id}`});
+    res.json({msg:`Hello from category patch route ID ${id}`});
 });
 
-//delete single user
+//delete single category
 router.delete("/:id",(req,res)=>{
     console.log(req.params.id);
     //database operation
-    res.json({msg:"Hello from delete user route"});
+    res.json({msg:"Hello from delete category route"});
 });
 
 
